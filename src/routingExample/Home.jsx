68be70dd458e1ab740c76ad99b2cdc05d430151a
IshankaDK <ishanka.d@industrialsoftwaregroup.com>
2023-02-22 00:20:42 +0530

    wrap a button with Link

--- src/routingExample/Home.jsx
+++ src/routingExample/Home.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Button } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
-    <div>Home</div>
-    <Link to={`about`}>to about page</Link> <br />
-    <Link to={`/contact`}>to contact page</Link>
+      <div>Home</div>
+      <Link to={`about`}>
+        <Button variant="contained" disabled={false} color={"warning"}>
+        to about pag
+        </Button>
+      </Link>
+      <br />
+      {/* <Link to={`about`}>to about page</Link> <br /> */}
+      <Link to={`/contact`}>to contact page</Link>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
